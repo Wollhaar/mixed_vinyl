48b19b8f2935e36893cccf7461fe19cea5b08a01
davidgoraj <david.goraj@cec.valantic.com>
2022-09-05 15:46:21 +0200

    created some twig content

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -3,16 +3,37 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class VinylController
+class VinylController extends AbstractController
 {
     #[Route('/')]
+    public function homepage(): Response
+    {
+        $tracks = [
+            ['song' => 'September', 'artist' => 'Earth, Wind and Fire'],
+            ['song' => 'Hurt', 'artist' => 'Johnny Cash'],
+            ['song' => 'Lonely Day', 'artist' => 'System of a Down'],
+            ['song' => 'Mindestens in 1000 Jahren', 'artist' => 'Frittenbude'],
+            ['song' => 'Teenage Dirtbag', 'artist' => 'Wheatus'],
+            ['song' => 'On Maelancholy Hill', 'artist' => 'Gorillaz'],
+        ];
+
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'Home of Vinyls',
+            'tracks' => $tracks
+        ]);
+    }
+
+    #[Route('/favorites')]
     public function favorites(): Response
     {
-        return new Response('<h1>Favorite Vinyls</h1>');
+        return $this->render('vinyl/favorites.html.twig', [
+            'title' => 'Favorite Vinyls'
+        ]);
     }
 
     #[Route('/categories/{category}')]
@@ -22,6 +43,9 @@ class VinylController
         if ($category) {
             $title = u(str_ireplace('-', ' ', $category))->title(true);
         }
-        return new Response('<h2>Vinyl Categories</h2><h3>' . $title . '</h3>');
+        return $this->render('vinyl/category.html.twig', [
+            'title' => 'Vinyl Categories',
+            'subtitle' => $title
+        ]);
     }
 }
